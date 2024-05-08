8d4a4d27e5331b4e25a24ea6cf7d98fadf265f8c
carlos.santos-seisdedos <carlos.santos-seisdedos@renault.com>
2024-05-08 17:05:45 +0200

    1D-robot python files reorganization

--- python/CruiseControl/PIDController.py
+++ python/CruiseControl/PIDController.py
@@ -5,42 +5,44 @@ class PIDController:
         self.Ki = Ki
         self.dt = dt
         # initial state
-        self.position_error = 0
+        self.error = 0
         self.integral_error = 0
+        # for plotting
         self.proportional_controls = []
         self.derivative_controls = []
         self.integrative_controls = []
         self.control_outputs = []
-    
-    def control(self, position_desired, position_current):
+
+    def control(self, setpoint, measurement):
         # Compute errors
-        position_error = position_error = position_desired - position_current
+        error = setpoint - measurement
 
         # Proportional term
-        P = self.Kp*position_error
+        P = self.Kp*error
         self.proportional_controls.append(P)    # for plotting
+
         # Derivative term
-        D = self.Kd * (position_error - self.position_error) / self.dt
+        D = self.Kd*(error-self.error)/self.dt
         self.derivative_controls.append(D)      # for plotting
-        # Integral term
-        self.integral_error += position_error*self.dt
-        I = self.Ki * self.integral_error
+        
+        # Integrative term
+        self.integral_error += error*self.dt
+        I = self.Ki*self.integral_error
         self.integrative_controls.append(I)     # for plotting
 
         # Update previous error
-        self.position_error = position_error
-        
+        self.error = error
+
         # Control signal output
         output = P+D+I
         self.control_outputs.append(output)     # for plotting
         return output
 
     def get_state(self):
-        return self.position_error, self.velocity_error, self.acceleration_error
+        return self.error, self.control_outputs[-1]
     
     def print_state(self, simulation_time):
-        # print("t=", simulation_time, " >>> position_error=", self.position_error, ", velocity_error=", self.velocity_error, ", acceleration_error=", self.acceleration_error)
-        print("t={:.2f} >>> PID: \t e={:.2f} (m), \t u={:.2f} (m/s)".format(simulation_time, self.position_error, self.control_outputs[-1]))
+        print("t={:.2f} >>> PID: \t e={:.2f} (m), \t u={:.2f} (m/s)".format(simulation_time, self.error, self.control_outputs[-1]))
 
     def get_proportional_controls(self):
         return self.proportional_controls
